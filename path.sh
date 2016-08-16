75e73244a81b78b47d83e5ce39cb760d2a9619f5
changzhisun <51141201057@stu.ecnu.edu.cn>
2016-08-16 16:45:01 +0800

    add multiopinexpr into .bashrc

--- path.sh
+++ path.sh
@@ -4,3 +4,4 @@ echo '''export PYTHONPATH=$OPIE_DIR/src:$PYTHONPATH''' >> $HOME/.bashrc
 echo '''export PYTHONPATH=$OPIE_DIR/src/my_package/relation:$PYTHONPATH''' >> $HOME/.bashrc
 echo '''export PYTHONPATH=$OPIE_DIR/src/my_package/complex:$PYTHONPATH''' >> $HOME/.bashrc
 echo '''export PYTHONPATH=$OPIE_DIR/src/my_package/utils:$PYTHONPATH''' >> $HOME/.bashrc
+echo '''export PYTHONPATH=$OPIE_DIR/src/my_package/multiopinexpr:$PYTHONPATH''' >> $HOME/.bashrc
